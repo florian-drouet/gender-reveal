595344613784fee0a6f9981abe70c8bcab58d453
florian-drouet <50357200+florian-drouet@users.noreply.github.com>
2021-11-25 12:55:08 +0100

    feat: add preprocessing

---
+++ func/name_to_gender.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import pandas as pd
+import unidecode
+
+def delete_accent(word: str) -> str:
+    """Delete accents in a string"""
+    return unidecode.unidecode(word)
+
+def check_sex(gender: str) -> str:
+    """Checks the gender of a person and returns 'indetermine' if the gender can be both male and female"""
+    if gender == 'm,f' or gender == 'f,m':
+        return "indetermine"
+    else:
+        return gender
+    
+def make_dictionary(dataframe):
+    """
+    Input : prenom.csv as a pandas dataframe
+    Output : a python dictionary {"PRENOM: SEX"}
+    """
+    
+    # Type of objects in the two first columns is string and we apply preprocessing functions
+    dataframe['01_prenom']=dataframe['01_prenom'].apply(lambda x: str(x))
+    dataframe["02_genre"]=dataframe["02_genre"].apply(lambda x: str(x))
+    dataframe['01_prenom']=dataframe['01_prenom'].apply(delete_accent)
+    dataframe["02_genre"]=dataframe["02_genre"].apply(check_sex)
+
+    # We return the two first table into a dictionary {"PRENOM: SEX"}
+    return dict([(i,a) for i, a in zip(dataframe["01_prenom"], dataframe["02_genre"])])
+
+def get_sex_from_name(liste_name_test: list, first_name_table: dict) -> list:
+    """
+    Input : the first names as a python list
+    Output : the corresponding sexes as a python list. If the first name is not found in the dictionary then it will add
+    'Indetermine' to the list.
+    """
+    column_sex = []
+    for index, prenom in enumerate(liste_name_test):
+        try:
+            column_sex.append(first_name_table[delete_accent(liste_name_test[index].lower())])
+        except:
+            column_sex.append("indetermine")
+    return column_sex
+
+if __name__ == '__main__':
+    # the file prenoms.csv in ANSI encoded
+    df = pd.read_csv("prenoms.csv", sep=";", encoding='ANSI')
+    first_name_table = make_dictionary(df)
+    dict_vocabulary = {"m": "male", "f": "female", "indetermine": "unclear"}
+    while True:
+        name = input('Enter your name:')
+        liste_name_test = [name]
+        column_sex = get_sex_from_name(liste_name_test, first_name_table)
+        print("Your gender is : "+str(dict_vocabulary[column_sex[0]])+"\n")
---
+++ func/text_preprocessing.py
@@ -0,0 +1,27 @@
+class TextPreprocessing:
+    
+    def __init__(self, serie):
+        self.serie = serie
+    
+    def lower(self):
+        self.serie = self.serie.str.lower()
+        return self
+        
+    def normalize(self):
+        self.serie = self.serie.str.normalize('NFKD').str.encode('ascii', errors='ignore').str.decode('utf-8')
+        return self
+        
+    def regex_cleaner(self):
+        self.serie = self.serie.str.replace('[^\w\s]', ' ', regex=True)
+        return self
+    
+    @staticmethod
+    def cln_whtspcs(sentence):
+        if sentence is None:
+            return None
+        else:
+            return ' '.join(str(sentence).split())
+    
+    def clean_whitespaces(self):
+        self.serie = self.serie.apply(self.cln_whtspcs)
+        return self
