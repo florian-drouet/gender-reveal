e22176b06b980c48a6df13ee000735a723d536f1
florian-drouet <50357200+florian-drouet@users.noreply.github.com>
2021-11-25 12:54:43 +0100

    feat: add preprocessing

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+import pandas as pd
+from func.preprocess_insee_file import clean_insee_file, save_file
 
+# original source : https://www.insee.fr/fr/statistiques/2540004?sommaire=4767262
+insee_file = pd.read_csv("local_data/nat2020.csv", sep=';')
+
+prenoms = clean_insee_file(insee_file=insee_file)
+save_file(prenoms)
