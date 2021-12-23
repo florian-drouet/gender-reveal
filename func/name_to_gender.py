3447f88e45b4e23e4152754198e67deead829a59
florian-drouet <50357200+florian-drouet@users.noreply.github.com>
2021-12-23 12:04:23 +0100

    fix: change man to male everywhere

--- func/name_to_gender.py
+++ func/name_to_gender.py
@@ -7,10 +7,10 @@ import pandas as pd
 def get_gender_from_name(
     name_to_classify: str, threshold: int, names: pd.DataFrame
 ) -> str:
-    proba_man = names.loc[(names.firstname == name_to_classify)]["proba_man"].values
-    if proba_man >= threshold:
+    proba_male = names.loc[(names.firstname == name_to_classify)]["proba_male"].values
+    if proba_male >= threshold:
         return "male"
-    elif proba_man <= 100 - threshold:
+    elif proba_male <= 100 - threshold:
         return "female"
     else:
         return "unclear"
--- func/preprocess_insee_file.py
+++ func/preprocess_insee_file.py
@@ -29,12 +29,12 @@ def clean_insee_file(insee_file):
         .fillna(0.0)
     )
     firstnames_clean = firstnames_clean.rename(
-        columns={"preusuel_clean": "firstname", 1: "proba_man", 2: "proba_female"}
+        columns={"preusuel_clean": "firstname", 1: "proba_male", 2: "proba_female"}
     )
 
-    firstnames_clean["total"] = firstnames_clean.proba_man + firstnames_clean.proba_female
-    firstnames_clean["proba_man"] = round(
-        firstnames_clean.proba_man.div(firstnames_clean.total).mul(100), 2
+    firstnames_clean["total"] = firstnames_clean.proba_male + firstnames_clean.proba_female
+    firstnames_clean["proba_male"] = round(
+        firstnames_clean.proba_male.div(firstnames_clean.total).mul(100), 2
     )
     firstnames_clean["proba_female"] = round(
         firstnames_clean.proba_female.div(firstnames_clean.total).mul(100), 2
@@ -46,7 +46,7 @@ def clean_insee_file(insee_file):
     # the 'popularity' of the firstname is set to at least 1000 occurences
     firstnames_clean = firstnames_clean.loc[(firstnames_clean.firstname_length>=3) & (firstnames_clean.total>=1000)].dropna()
 
-    return firstnames_clean[["firstname", "firstname_length", "proba_man", "proba_female", "total"]].sort_values(by=["firstname_length", "total"], ascending=False).reset_index(drop=True)
+    return firstnames_clean[["firstname", "firstname_length", "proba_male", "proba_female", "total"]].sort_values(by=["firstname_length", "total"], ascending=False).reset_index(drop=True)
 
 
 def save_file(file):
